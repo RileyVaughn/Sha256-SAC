0ef30c4c6f13626c8833fbdd5029598f7c6c805f
RileyVaughn <rj.vaughn@live.com>
2024-01-30 16:29:34 -0700

    init_vals added

---
+++ init_vals/init.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+func main() {
+	N := 10 //1000000
+	var msgs [][]string
+	for i := 0; i < N; i++ {
+		msgs = append(msgs, generateMsg())
+	}
+	Write("init_vals_512", msgs)
+}
+
+func Write(filename string, data [][]string) {
+
+	f, err := os.Create("./init_vals/" + filename + ".csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	writer := csv.NewWriter(f)
+
+	err = writer.WriteAll(data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+// 512-bit message
+func generateMsg() []string {
+	var msg [16]uint32
+	for i := 0; i < 16; i++ {
+		msg[i] = rand.Uint32()
+	}
+	msg_str := strings.Split(strings.Trim(fmt.Sprint(msg), "[]"), ",")
+
+	return msg_str
+}
